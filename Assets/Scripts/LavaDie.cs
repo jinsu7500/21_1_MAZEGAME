2a0d742ce85a8385929f91fd77da8c519b26eacb
Park jinsu <bluesky32132489@gmail.com>
2021-05-12 10:05:45 +0900

    2021.05.12
    R5추가, LavaDie 텔포로 수정

--- Assets/Scripts/LavaDie.cs
+++ Assets/Scripts/LavaDie.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class LavaDie : MonoBehaviour
 {
-    public GameOverScreen GameOverScreen;
+    //public GameOverScreen GameOverScreen;
+    public GameObject DieMessage;
+    public GameObject Player;
+    public Transform teleportPos;
+
+    private bool isPlayerAlive = true;
+
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            GameOverScreen.Setup();
+            //GameOverScreen.Setup();
+            DieMessage.SetActive(true);
+            isPlayerAlive = false;
+
+            //움직임 고정
+            Player.GetComponent<PlayerController>().enabled = false;
             Debug.Log("플레이어 사망");
     
+        }
+    }
+
+    public void Update()
+    {
+        if (!isPlayerAlive)
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                Player.GetComponent<PlayerController>().enabled = true;
+                isPlayerAlive = true;
+                DieMessage.SetActive(false);
+                //SceneManager.LoadScene("Round2_1");
+                CharacterController cc = Player.GetComponent<CharacterController>();
+
+                cc.enabled = false;
+                Player.transform.position = teleportPos.transform.position;
+                cc.enabled = true;
+                Debug.Log("텔포");
+
+                
+            }
         }
     }
 }
